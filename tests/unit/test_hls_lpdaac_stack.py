e0c3ba054fe58b4dd24f17abdfa0e9ca11862b47
Chuck Daniels <chuck@developmentseed.org>
2021-12-13 09:10:23 -0500

    Initial commit

---
+++ tests/unit/test_hls_lpdaac_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from hls_lpdaac.hls_lpdaac_stack import HlsLpdaacStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in hls_lpdaac/hls_lpdaac_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = HlsLpdaacStack(app, "hls-lpdaac")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
