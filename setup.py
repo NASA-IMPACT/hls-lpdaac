19e90797d96a2c6510e7a3f486c6775f65437dbe
Chuck Daniels <chuck@developmentseed.org>
2025-03-19 13:22:51 -0400

    Bump package version number and min python (#23)

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ extras_require = {
 
 setup(
     name="hls-lpdaac",
-    version="0.0.1",
-    python_requires=">=3.9",
+    version="1.1",
+    python_requires=">=3.12",
     author="Development Seed",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
