f88d1bece5638743e3a4d77441d6826d1b8e1b8e
Chris Holden <ceholden@users.noreply.github.com>
2026-01-13 16:49:29 -0500

    fix: remove sending to tiler queue (#27)

--- src/hls_lpdaac/forward/index.py
+++ src/hls_lpdaac/forward/index.py
@@ -16,13 +16,12 @@ def handler(event: "S3Event", _: "Context") -> None:
     return _handler(  # pragma: no cover
         event,
         lpdaac_queue_url=os.environ["LPDAAC_QUEUE_URL"],
-        tiler_queue_url=os.environ["TILER_QUEUE_URL"],
     )
 
 
 # Enables unit testing without the need to monkeypatch `os.environ` (which would
 # be necessary to test `handler` above).
-def _handler(event: "S3Event", *, lpdaac_queue_url: str, tiler_queue_url: str) -> None:
+def _handler(event: "S3Event", *, lpdaac_queue_url: str) -> None:
     # The S3Event type is not quite correct, so we are forced to ignore a couple
     # of typing errors that would not occur if the type were defined correctly.
     s3_object = event["Records"][0]["s3"]  # type: ignore
@@ -31,12 +30,6 @@ def _handler(event: "S3Event", *, lpdaac_queue_url: str, tiler_queue_url: str) -
     json_key = s3_object["object"]["key"]  # type: ignore
     json_contents = s3.Object(bucket, json_key).get()["Body"].read().decode("utf-8")
     _send_message(lpdaac_queue_url, key=json_key, message=json_contents)
-
-    # Send message to tiler queue ONLY for non-VI files
-    if "_VI/" not in json_key:
-        stac_json_key = json_key.replace(".json", "_stac.json")
-        stac_url = f"s3://{bucket}/{stac_json_key}"
-        _send_message(tiler_queue_url, key=stac_json_key, message=stac_url)
 
 
 def _send_message(queue_url: str, *, key: str, message: str) -> None:
